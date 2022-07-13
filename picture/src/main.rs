7ad508c2be7d9f193c9a38327a1587c0eba04c66
Savoury SnaX <savoury.snax@googlemail.com>
2022-07-13 12:43:31 +0100

    Cleanup add initial plot pen support fix height for images

--- picture/src/main.rs
+++ picture/src/main.rs
@@ -6,27 +6,44 @@ use std::vec;
 use dir_resource::{ResourceDirectory, ResourceDirectoryEntry};
 use volume::Volume;
 
+struct Root<'a> {
+    base_path:&'a Path,
+}
+
+impl<'a> Root<'_> {
+    pub fn new(base_path:&'a str) -> Root {
+        Root {base_path:Path::new(base_path)}
+    }
+}
+
 fn main() {
-    let bytes = fs::read("../images/Space Quest- The Sarien Encounter v1.0X (1986)(Sierra On-Line, Inc.) [Adventure]/PICDIR").unwrap_or_default();
+
+    let root = Root::new("../images/Leisure Suit Larry in the Land of the Lounge Lizards (1987)(Sierra On-Line, Inc.) [Adventure]/");
+    //let root = Root::new("../images/Space Quest- The Sarien Encounter v1.0X (1986)(Sierra On-Line, Inc.) [Adventure]/");
+
+    let bytes = fs::read(root.base_path.join("PICDIR").into_os_string()).unwrap_or_default();
 
     let dir = ResourceDirectory::new(bytes.into_iter()).unwrap();
 
     for (index,entry) in dir.into_iter().enumerate() {
         if !entry.empty() {
             println!("{} : V{} P{}",index,entry.volume,entry.position);
-            dump_picture_resource(&entry, index);
+            dump_picture_resource(&root,&entry, index);
         }
     }
 
 }
 
-fn dump_picture_resource(entry:&ResourceDirectoryEntry, index:usize) {
+const WIDTH:i16 = 160;
+const HEIGHT:i16 = 168;
 
-    let bytes = fs::read(format!("../images/Space Quest- The Sarien Encounter v1.0X (1986)(Sierra On-Line, Inc.) [Adventure]/VOL.{}", entry.volume)).unwrap_or_default();
+fn dump_picture_resource(root:&Root,entry:&ResourceDirectoryEntry, index:usize) {
+
+    let bytes = fs::read(root.base_path.join(format!("VOL.{}", entry.volume)).into_os_string()).unwrap_or_default();
 
     let volume = Volume::new(bytes.into_iter()).unwrap();
 
-    let picture = match process_picture(&volume,entry) {
+    let picture = match process_picture(index, false, &volume,entry) {
         Ok(b) => b,
         Err(s) => panic!("Failed due to : {}", s),
     };
@@ -35,15 +52,15 @@ fn dump_picture_resource(entry:&ResourceDirectoryEntry, index:usize) {
 
     let rgba = conv_rgba(&doubled_width);
 
-    dump_png(format!("../{}-picture.png",index).as_str(),320,200,&rgba);
+    dump_png(format!("../{}-picture.png",index).as_str(),(WIDTH*2) as u32,HEIGHT as u32,&rgba);
 
     let doubled_width = double_width(&&&picture.priority);
 
     let rgba = conv_greyscale(&doubled_width);
 
-    dump_png(format!("../{}-priority.png",index).as_str(),320,200,&rgba);
+    dump_png(format!("../{}-priority.png",index).as_str(),(WIDTH*2) as u32,HEIGHT as u32,&rgba);
 
-    let mut volume_iter = fetch_data_iterator(&volume, entry);
+    let volume_iter = fetch_data_iterator(&volume, entry);
 
     let data:Vec<u8> = volume_iter.cloned().collect();
     fs::write(format!("../{}-binary.bin",index).as_str(),data).unwrap();
@@ -68,10 +85,10 @@ fn fetch_data_iterator<'a>(volume: &'a Volume, entry: &ResourceDirectoryEntry) -
 
 
 // Attach to volume manager - get picture resource...?
-fn process_picture(volume:&Volume, entry: &ResourceDirectoryEntry) -> Result<PictureResource, String> {
+fn process_picture(index:usize, dump_parts:bool, volume:&Volume, entry: &ResourceDirectoryEntry) -> Result<PictureResource, String> {
 
-    let mut picture = vec![15u8;160*200];
-    let mut priority = vec![4u8;160*200];
+    let mut picture = vec![15u8;(WIDTH*HEIGHT) as usize];
+    let mut priority = vec![4u8;(WIDTH*HEIGHT) as usize];
 
     let mut volume_iter = fetch_data_iterator(volume, entry).peekable();
 
@@ -79,6 +96,12 @@ fn process_picture(volume:&Volume, entry: &ResourceDirectoryEntry) -> Result<Pic
     let mut priority_pen=4u8;
     let mut colour_on=false;
     let mut priority_on=false;
+
+    let mut last_match=0u8;
+    let mut part_index = 0;
+    let mut plot_pen_size:u8 = 0;
+    let mut plot_pen_splatter=false;
+    let mut plot_pen_rectangle:bool=true;
 
     while let Some(b) = volume_iter.next() {
         match b {
@@ -91,86 +114,41 @@ fn process_picture(volume:&Volume, entry: &ResourceDirectoryEntry) -> Result<Pic
             0xF6 => { absolute_line(&mut picture,&mut priority,colour_on,priority_on,colour_pen,priority_pen,&mut volume_iter); },
             0xF7 => { relative_line(&mut picture,&mut priority,colour_on,priority_on,colour_pen,priority_pen,&mut volume_iter); },
             0xF8 => { fill(&mut picture,&mut priority,colour_on,priority_on,colour_pen,priority_pen,&mut volume_iter); },
+            0xFA => { plot_pen(&mut picture,&mut priority,colour_on,priority_on,colour_pen,priority_pen,plot_pen_size,plot_pen_splatter,plot_pen_rectangle,&mut volume_iter); },
 
             0xFF => break,
             _ => return Err(format!("Unhandled control code {:02X}",b)),
+        }
+
+        if *b != last_match && dump_parts {
+
+            let doubled_width = double_width(&picture);
+
+            let rgba = conv_rgba(&doubled_width);
+
+            dump_png(format!("../{}-picture-{}.png",index,part_index).as_str(),(WIDTH*2) as u32,HEIGHT as u32,&rgba);
+
+            let doubled_width = double_width(&priority);
+
+            let rgba = conv_greyscale(&doubled_width);
+
+            dump_png(format!("../{}-priority-{}.png",index,part_index).as_str(),(WIDTH*2) as u32,HEIGHT as u32,&rgba);
+
+            part_index = part_index+1;
+            last_match=*b;
         }
     }
 
     return Ok(PictureResource {picture,priority});
 }
 
-fn rasterise_round(num:f64,dirn:f64) -> usize {
-    unsafe {
-        if dirn < 0.0 {
-            return if (num - num.floor())<=0.501 {num.floor() as usize} else {num.ceil() as usize};
-        } else {
-            return if (num - num.floor())< 0.499 {num.floor() as usize} else {num.ceil() as usize};
-        }
-    }
-}
-
-fn rasterise_line_shit(picture:&mut Vec<u8>,priority:&mut Vec<u8>,colour_on:bool,priority_on:bool,colour_pen:u8,priority_pen:u8, x0:i16, y0:i16, x1:i16, y1:i16) {
-
-    let height:i32=(y1-y0).into();
-    let width:i32=(x1-x0).into();
-
-    let mut x=0.0;
-    let mut y=0.0;
-    let mut dx = if height==0 {height as f64} else {width as f64 / (height as f64).abs() };
-    let mut dy = if width==0 {width as f64} else {height as f64 / (width as f64).abs() };
-
-    if width.abs() > height.abs() {
-        x = x0 as f64;
-        y = y0 as f64;
-        dx = if width==0 {width as f64} else {width as f64 / (width as f64).abs() };
-        while x<x1.into() {
-            let coord:usize = 160*rasterise_round(y,dy) + rasterise_round(x,dx);
-            if colour_on {
-                picture[coord]=colour_pen;
-            }
-            if priority_on {
-                priority[coord]=priority_pen;
-            }
-            
-            x = x + dx;
-            y = y + dy;
-        }
-    } else {
-        x = x0 as f64;
-        y = y0 as f64;
-        dy = if height==0 {height as f64} else {height as f64 / (height as f64).abs() };
-        while y<y1.into() {
-            let coord:usize = 160*rasterise_round(y,dy) + rasterise_round(x,dx);
-            if colour_on {
-                picture[coord]=colour_pen;
-            }
-            if priority_on {
-                priority[coord]=priority_pen;
-            }
-
-            x = x + dx;
-            y = y + dy;
-        }
-    }
-
-    let coord:usize = (160*y1 + x1) as usize;
-    if colour_on {
-        picture[coord]=colour_pen;
-    }
-    if priority_on {
-        priority[coord]=priority_pen;
-    }
-
-}
-
 fn rasterise_plot(picture:&mut Vec<u8>,priority:&mut Vec<u8>,colour_on:bool,priority_on:bool,colour_pen:u8,priority_pen:u8, x:i16, y:i16) {
 
-    if x<0 || x>159 || y<0 || y>199 {
+    if x<0 || x>(WIDTH-1) || y<0 || y>(HEIGHT-1) {
         return;
     }
 
-    let coord = (160*y+x) as usize;
+    let coord = (WIDTH*y+x) as usize;
     if colour_on {
         picture[coord]=colour_pen;
     }
@@ -179,6 +157,25 @@ fn rasterise_plot(picture:&mut Vec<u8>,priority:&mut Vec<u8>,colour_on:bool,prio
     }
 
 }
+
+fn rasterise_plot_pen(picture:&mut Vec<u8>,priority:&mut Vec<u8>,colour_on:bool,priority_on:bool,colour_pen:u8,priority_pen:u8,plot_pen_size:u8,plot_pen_splatter:bool,plot_pen_rectangle:bool,x:i16,y:i16) {
+
+    // pen size 0-7
+    if plot_pen_size != 0 {
+        panic!("Pen Sizes > not supported");
+    }
+
+    if plot_pen_splatter {
+        panic!("Pen Splatter not supported");
+    }
+
+    if !plot_pen_rectangle {
+        panic!("Circle not supported");
+    }
+
+    rasterise_plot(picture, priority, colour_on, priority_on, colour_pen, priority_pen, x, y);
+}
+
 
 fn rasterise_line(picture:&mut Vec<u8>,priority:&mut Vec<u8>,colour_on:bool,priority_on:bool,colour_pen:u8,priority_pen:u8, x0:i16, y0:i16, x1:i16, y1:i16) {
 
@@ -232,14 +229,14 @@ fn rasterise_fill(picture:&mut Vec<u8>,priority:&mut Vec<u8>,colour_on:bool,prio
         let (x,y) = queue.pop_front().unwrap();
 
         let vec_coord = y as usize;
-        let vec_coord = vec_coord * 160;
+        let vec_coord = vec_coord * (WIDTH as usize);
         let vec_coord: usize = vec_coord + x as usize;
 
         if colour_on && picture[vec_coord]!=15 {
             continue;
         }
 
-        if priority_on && priority[vec_coord]!=4 {
+        if priority_on && !colour_on && priority[vec_coord]!=4 {
             continue;
         }
 
@@ -250,10 +247,10 @@ fn rasterise_fill(picture:&mut Vec<u8>,priority:&mut Vec<u8>,colour_on:bool,prio
             priority[vec_coord]=priority_pen;
         }
 
-        if x<159 { queue.push_back((x+1,y)); }
-        if x>0   { queue.push_back((x-1,y)); }
-        if y<199 { queue.push_back((x,y+1)); }
-        if y>0   { queue.push_back((x,y-1)); }
+        if x<((WIDTH-1) as u8)  { queue.push_back((x+1,y)); }
+        if x>0                  { queue.push_back((x-1,y)); }
+        if y<((HEIGHT-1) as u8) { queue.push_back((x,y+1)); }
+        if y>0                  { queue.push_back((x,y-1)); }
     }
 
 }
@@ -364,10 +361,32 @@ where I: Iterator<Item = &'a u8> {
 
         println!("Fill at : {},{}",x,y);
 
-        rasterise_fill(picture, priority, colour_on, priority_on, colour_pen, priority_pen, *x, *y);
+        if (colour_on || priority_on) {
+            rasterise_fill(picture, priority, colour_on, priority_on, colour_pen, priority_pen, *x, *y);
+        }
     }
 }
 
+fn plot_pen<'a, I>(picture:&mut Vec<u8>,priority:&mut Vec<u8>,colour_on:bool,priority_on:bool,colour_pen:u8,priority_pen:u8,plot_pen_size:u8,plot_pen_splatter:bool,plot_pen_rectangle:bool,volume_iter:&mut Peekable<I>)
+where I: Iterator<Item = &'a u8> {
+
+    if plot_pen_splatter {
+        panic!("Splatter pen not implemented");
+    }
+
+    while let Some(b) = volume_iter.peek() {
+        if **b >= 0xF0 {
+            return;
+        }
+        let x = volume_iter.next().unwrap();
+        let y = volume_iter.next().unwrap();
+
+        println!("Plot Pen at : {},{}",x,y);
+
+        rasterise_plot_pen(picture, priority, colour_on, priority_on, colour_pen, priority_pen, plot_pen_size, plot_pen_splatter, plot_pen_rectangle, (*x).into(), (*y).into());
+    }
+
+}
 
 fn double_width(data:&Vec<u8>) -> Vec<u8> {
     let mut out_vec = Vec::new();
